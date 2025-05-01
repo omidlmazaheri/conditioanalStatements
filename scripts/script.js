d7ae8e7b1726c673033acb81a9962073e1a34ac0
omidlmazaheri <omidlmazaheri@gmail.com>
2025-05-01 14:59:04 +0330

    case insensitive login username

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,14 @@
 
-let uname, psw;
+let uname, psw , msg;
+uname=document.getElementById("username");
+psw=document.getElementById("password");
+msg=document.getElementById("prompt");
+// case insensetive
 function login() {
-    alert("login");
+    if (uname.value.toLowerCase()==="admin".toLowerCase() && psw.value==="123" ) {
+        msg.innerText="welcome";
+    } else {
+        msg.innerText="something goes wrong!";
+
+    }
 }
