b930a4d6587796809e7d737707b9dcfc057d85af
omidlmazaheri <omidlmazaheri@gmail.com>
2025-05-02 15:10:38 +0330

    add return to function

---
+++ scripts/area.js
@@ -0,0 +1,20 @@
+
+let objWidth=document.getElementById("width");
+let objHeight=document.getElementById("height");
+let objResult=document.getElementById("result");
+let widthValue, heightValue,areaValue; result;
+
+
+function calculateArea() {
+    widthValue=objWidth.value;
+    heightValue=objHeight.value;
+    result= widthValue* heightValue;
+    return result;
+
+}
+function showArea(){
+    areaValue= calculateArea();
+    // OMID: Template Literals
+
+    objResult.innerHTML=`the area is <b> ${areaValue} <b>`;
+}
