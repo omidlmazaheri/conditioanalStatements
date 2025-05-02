03ac929fe573c89537e2f59c6d46e9382357de95
omidlmazaheri <omidlmazaheri@gmail.com>
2025-05-02 14:02:37 +0330

    add grade

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,9 @@
 
 let uname, psw;
+let score, getScore, objGrade;
+objScore=document.getElementById("score");
+objGrade=document.getElementById("grade");
+
 function login() {
     if (uname.value.toLowerCase()==="admin".toLowerCase() && psw.value==="123" ) {
         msg.innerText="welcome";
@@ -8,3 +12,23 @@ function login() {
 
     }
 }
+
+function studentGrade(){
+    debugger
+    score= objScore.value;
+    if (score >=90 || score<100) {
+        objGrade.innerText="Your Grade is A";
+    } else if(score >=80 || score<90){
+        objGrade.innerText="Your Grade is B";
+
+    } else if(score >=50 || score<80){
+        objGrade.innerText="Your Grade is C";
+
+    }else{
+        objGrade.innerText="Your Grade is F";
+
+    }
+}
+
+
+
